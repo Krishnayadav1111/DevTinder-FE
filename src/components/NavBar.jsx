393280e5d47e64757edb7721c7573a0589abb674
Krishnayadav1111 <krishnantv1111@gmail.com>
2025-12-27 21:25:53 +0530

    updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,7 +25,7 @@ const NavBar = () => {
     <>
       <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <Link to="/" className="btn btn-ghost text-xl">🎙️Dev Tinder</Link>
+          <Link to="/" className="btn btn-ghost text-xl">Be special</Link>
         </div>
         {user && <div className="flex gap-2">
           <div className='mr-2'> Welcome {user?.firstName}</div>
